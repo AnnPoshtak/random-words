be76fcede02a4adc4dc0f318704919e38022222e
sad1dab <a.poshtak2011@gmail.com>
2025-05-29 14:12:39 +0300

    Оновлення 29.05.25 14:12

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 document.getElementById('generate').addEventListener('click', function () {
     const words = document.getElementById("words").value
+    const howManyRandomWords = document.getElementById("How-many-words").value;
     const array =  words.split(/[\s,]+/).map(word => word.trim().toLowerCase()).filter(word => word.length > 0);
-    document.querySelectorAll('.array_of_words').forEach(el => el.innerHTML = '')
-    for (let i = 0; i < array.length; i++) {
+    document.querySelectorAll('.array_of_words').forEach(el => el.innerHTML = '');
+    function mixWords(array) {
+        const index1 = Math.floor(Math.random() * array.length);
+        const index2 = Math.floor(Math.random() * array.length);
+
+        let word = array[index1];
+        array[index1] = array[index2];
+        array[index2] = word;
+
+        return array;
+    }
+    for (let j = 0; j < 5; j++) {
+        mixWords(array);
+    }
+    for (let i = 0; i < howManyRandomWords; i++) {
         let newDiv = document.createElement('div');
         newDiv.textContent = array[i];
         newDiv.classList.add('array_of_words');
         document.body.appendChild(newDiv)
     }
+
 })
